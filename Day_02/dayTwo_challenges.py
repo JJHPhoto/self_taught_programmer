1a650f828e13805305b01ece18f4a7c46bfd988a
Joshua J. Haller <phone@joshuajhaller.com>
2023-11-05 18:09:59 +0700

    Finished the challenges with a nod to Big Bang.

--- Day_02/dayTwo_challenges.py
+++ Day_02/dayTwo_challenges.py
@@ -21,3 +21,30 @@
 #   print("I'm smaller than 10!")
 # elif x >= 10:
 #   print("Bigger than 10!")
+
+# x = 90
+
+# if x < 10:
+#   print('Gonna need to get bigger!')
+# elif x > 10 and x < 25:
+#   print('Stuck in between!')
+# elif x > 25:
+#   print('Way too big!')
+
+# x = 12
+# y = 5
+# print(x%y)
+# print(x//y)
+
+age = 42
+
+if age == 20:
+  print('Almost legally allowed to have fun!')
+elif age == 25:
+  print('Halfway to 50!')
+elif age == 40:
+  print('Time to write your will!')
+elif age == 90:
+  print('What you still doing here?')
+else:
+  print('How drunk am I?')
