9af0ba216aa467c6bcc68f0a4a93ed41a3996850
Joshua J. Haller <phone@joshuajhaller.com>
2023-11-05 18:02:15 +0700

    Finished the Statements section and working through the Challenges for the chapter.

---
+++ Day_02/dayTwo_challenges.py
@@ -0,0 +1,23 @@
+# x = 45
+
+# if x == 20:
+#   print('20!')
+# elif x == 40:
+#   print('40!')
+# else:
+#   print("I don't know!")
+
+# if x % 2 == 0:
+#   print('x is even!')
+# else:
+#   print('x is odd!')
+
+# if x == 45:
+#   print('I knew that!')
+
+# x = 11
+
+# if x < 10:
+#   print("I'm smaller than 10!")
+# elif x >= 10:
+#   print("Bigger than 10!")
---
+++ Day_02/statements.py
@@ -0,0 +1,20 @@
+# print('Hello, World!')
+
+# for i in range(100):
+#   print('Hello, World!')
+
+x = 100
+if x == 10:
+  print('10!')
+elif x == 20:
+  print('20!')
+else:
+  print("I donn't know!")
+
+if x == 100:
+  print('x is 100!')
+
+if x % 2 == 0:
+  print('x is even!')
+else:
+  print('x is odd!')
